4e22def57ee6d9fcd21a29eff07273c20e5b0416
nishida <42875682+nishidayoshikatsu@users.noreply.github.com>
2023-12-29 10:55:30 +0900

    :wrench: Set gh-pages config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "production"
+      ? "/luma-three-testbed/"
+      : "/",
+    plugins: [react()],
+  }
+});
