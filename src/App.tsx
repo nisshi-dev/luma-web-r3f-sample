c6a2abcb5562e93aa6e0b06a80a838044869ee14
nishida <42875682+nishidayoshikatsu@users.noreply.github.com>
2024-05-11 18:06:17 +0900

    :lipstick: サンプルのデータに戻す

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,8 @@ import { VRButton, XR, Controllers, Hands } from "@react-three/xr";
 export const App = () => {
   const [autoRotate, setAutoRotate] = useState(true);
 
-  //const dataURL = "https://lumalabs.ai/capture/ca9ea966-ca24-4ec1-ab0f-af665cb546ff"
-  const dataURL = "https://lumalabs.ai/capture/250b6a1c-34da-475c-9c5b-90ba2df852fc"
+  const dataURL = "https://lumalabs.ai/capture/ca9ea966-ca24-4ec1-ab0f-af665cb546ff"
+  //const dataURL = "https://lumalabs.ai/capture/250b6a1c-34da-475c-9c5b-90ba2df852fc"
 
   return (
     <div className="w-[100vw] h-[100vh]">
