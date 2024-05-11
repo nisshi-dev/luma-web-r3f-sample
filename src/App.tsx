6cc63aa66c6f77742578b3c07cbd5bd62d153911
nishida <42875682+nishidayoshikatsu@users.noreply.github.com>
2024-05-11 17:10:45 +0900

    :bento: Change data

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ export const App = () => {
   const [autoRotate, setAutoRotate] = useState(true);
 
   //const dataURL = "https://lumalabs.ai/capture/ca9ea966-ca24-4ec1-ab0f-af665cb546ff"
-  const dataURL = "https://lumalabs.ai/capture/8661f25b-8c91-4c09-9c03-87fd6b490138"
+  const dataURL = "https://lumalabs.ai/capture/250b6a1c-34da-475c-9c5b-90ba2df852fc"
 
   return (
     <div className="w-[100vw] h-[100vh]">
